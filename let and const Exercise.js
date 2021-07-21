c3fae1b7ca3e0b01477855e2a6c98985d06fc5e4
Jimmy <jimjam5227@gmail.com>
2021-07-21 02:41:15 -0400

    first commit

---
+++ let and const Exercise.js
@@ -0,0 +1,28 @@
+// ES5 Global Constants
+
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+// ES2015 Global Constants
+/* Write an ES2015 Version */
+
+const PI = 3.14;
+
+// Quiz
+// What is the difference between var and let?
+
+// You can redeclare var but not let. The scope rules for var are defined
+// as a function scope whereas the scope rules for let is a block scope.
+
+// What is the difference between var and const?
+
+// You can reassign and redecalre var but not const. Var is also limited
+// to the function scope whereas const is a block scope.
+
+// What is the difference between let and const?
+
+// You can only reassign for let but not const.
+
+// What is hoisting?
+
+// All variable declarations are ran first before initialization.
